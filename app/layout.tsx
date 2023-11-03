1eefb777d2d0a9b30badad02636298f9bc0221ad
an.le6 <anle.drewindeed@gmail.com>
2023-11-03 14:34:13 +0700

    5. feat: theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -30,8 +33,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          enableSystem
+          disableTransitionOnChange
+          attribute="class"
+          defaultTheme="system"
+          storageKey="jotivate-2023"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
